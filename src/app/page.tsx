52f28ccab474d7d7918cc9cdb6c27cac1b4a15a1
zzuhann <zzuhann0125@gmail.com>
2023-11-25 23:21:34 +0800

    feat: add person card layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import PersonCard from '@/components/person-card/person-card'
 
 function Home() {
   return (
-    <div className="w-full h-full bg-black">
-      <div className="pt-[148px] pb-[48px] px-[16px] flex flex-col">
+    <div className="w-screen h-screen bg-black">
+      <div className="pt-[148px] pb-[48px] px-[16px] flex flex-col h-full w-full gap-4 sm:flex-row sm:gap-5">
+        <PersonCard />
+        <PersonCard />
         <PersonCard />
       </div>
     </div>
