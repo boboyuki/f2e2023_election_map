74e954897432a3aae8ed2757a3ea5ebd5b617825
pg <frankpg267@gmail.com>
2023-11-28 08:27:40 +0800

    feat: add map event and style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,15 @@
 import Map from '@/components/map/map';
-// import { Chart } from '../components/vote-chart/chart';
-// import { EPoliticalPartyId } from '../constants';
-// import TAIWAN_AREA from '../../../public/vote-data/taiwanArea.json';
+import { Chart } from '../components/vote-chart/chart';
+import { EPoliticalPartyId } from '../constants';
+import TAIWAN_AREA from '../../../public/vote-data/taiwanArea.json';
 
 export default function Home() {
   return (
-    <div className="w-3/5 h-screen pt-[100px]">
-      <Map></Map>
-
-      {/* <div className="p-10">
-        <Chart
-          politicalPartyId={EPoliticalPartyId.DPP}
-          cityId={TAIWAN_AREA.cities[0].id}
-          townId={TAIWAN_AREA.towns[0].id}
-        />
-      </div> */}
-
-      {/* <div className="flex">
+    <>
+      <div className="md:w-3/5 w-full md:h-screen h-[324px] pt-[100px]">
+        <Map />
+      </div>
+      <div className="md:w-2/5 w-full md:h-screen pt-[100px]">
         <div className="p-10">
           <Chart
             politicalPartyId={EPoliticalPartyId.DPP}
@@ -24,10 +17,20 @@ export default function Home() {
             townId={TAIWAN_AREA.towns[0].id}
           />
         </div>
-        <div className="p-10">
-          <Chart cityId={TAIWAN_AREA.cities[0].id} />
+
+        <div className="flex">
+          <div className="p-10">
+            <Chart
+              politicalPartyId={EPoliticalPartyId.DPP}
+              cityId={TAIWAN_AREA.cities[0].id}
+              townId={TAIWAN_AREA.towns[0].id}
+            />
+          </div>
+          <div className="p-10">
+            <Chart cityId={TAIWAN_AREA.cities[0].id} />
+          </div>
         </div>
-      </div> */}
-    </div>
+      </div>
+    </>
   );
 }
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,52 +1,357 @@
 'use client';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
-import { useEffect, useRef, useState } from 'react';
-import { FeatureCollection } from 'geojson';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { configResponsive, useResponsive } from 'ahooks/lib';
+import {
+  Feature,
+  FeatureCollection,
+  GeoJsonProperties,
+  Geometry,
+} from 'geojson';
 import { LEVEL } from '@/app/dashboard/lib/enum';
+import taiwanAreaJson from '../../../public/vote-data/taiwanArea.json';
+import voteDetailJson from '../../../public/vote-data/voteDetail.json';
+const taiwanArea = JSON.parse(JSON.stringify(taiwanAreaJson));
+const voteDetail = JSON.parse(JSON.stringify(voteDetailJson));
 const levelMap: Record<LEVEL, string> = {
   [LEVEL.COUNTY]: '/map-data/counties.json',
   [LEVEL.TOWN]: '/map-data/towns.json',
   [LEVEL.VILLAGE]: '/map-data/villages.json',
 };
+const colorMap = {
+  美丁美黨: '#FF8C22',
+  麥當黨: '#4299FF',
+  丁丁黨: '#79D46B',
+};
+configResponsive({
+  small: 0,
+  middle: 768,
+  large: 1280,
+});
+function getTitle(level: LEVEL, d: any) {
+  if (level === LEVEL.COUNTY) {
+    return d.properties?.['COUNTYNAME'];
+  } else if (level === LEVEL.TOWN) {
+    return d.properties?.['TOWNNAME'];
+  } else if (level === LEVEL.VILLAGE) {
+    return d.properties?.['VILLNAME'];
+  }
+  return '';
+}
+function getStyles(level: LEVEL) {
+  return function (d: any) {
+    const countyName = d.properties?.['COUNTYNAME'];
+    const townName = d.properties?.['TOWNNAME'];
+    const villageName = d.properties?.['VILLNAME'];
+    if (level === LEVEL.COUNTY) {
+      const data = voteDetail?.cities?.find(
+        (item: any) => item.city === countyName,
+      );
+      const highestVoteCandidate = data?.candidates?.sort(
+        (a: any, b: any) => b.voteRate - a.voteRate,
+      )[0]?.politicalPartyName as string;
+      return `fill: ${
+        colorMap[highestVoteCandidate as unknown as keyof typeof colorMap]
+      }; stroke: #000; stroke-width: 1px; opacity: 1; visibility: visible;`;
+    }
+    if (level === LEVEL.TOWN) {
+      const countyId = taiwanArea.cities.find(
+        (item: { name: string; id: string }) => item.name === countyName,
+      )?.id;
+      const data = voteDetail?.towns?.find(
+        (item: any) =>
+          item.id.split['-']?.[0] === countyId && item.town === townName,
+      );
+      const highestVoteCandidate = data?.candidates?.sort(
+        (a: any, b: any) => b.voteRate - a.voteRate,
+      )[0]?.politicalPartyName as string;
+      return `fill: ${
+        colorMap[highestVoteCandidate as unknown as keyof typeof colorMap]
+      }; stroke: #000; stroke-width: 1px; opacity: 1; visibility: visible;`;
+    }
+    if (level === LEVEL.VILLAGE) {
+      const countyId = taiwanArea.cities.find(
+        (item: { name: string; id: string }) => item.name === countyName,
+      )?.id;
+      const townId = taiwanArea.towns.find(
+        (item: { name: string; id: string }) =>
+          item.id.split('-')?.[0] === countyId && item.name === townName,
+      )?.id;
+      const data = voteDetail?.villages?.find(
+        (item: any) =>
+          item.id.split['-']?.[0] === countyId &&
+          item.id.split['-']?.[1] === townId &&
+          item.village === villageName,
+      );
+      const highestVoteCandidate = data?.candidates?.sort(
+        (a: any, b: any) => b.voteRate - a.voteRate,
+      )[0]?.politicalPartyName as string;
+      return `fill: ${
+        colorMap[highestVoteCandidate as unknown as keyof typeof colorMap]
+      }; stroke: #000; stroke-width: 1px; opacity: 1; visibility: visible;`;
+    }
+    return 'fill: #ccc; stroke: #000; stroke-width: 1px;';
+  };
+}
+function getRestClassNames(level: LEVEL) {
+  if (level === LEVEL.COUNTY) {
+    return ['town', 'village'];
+  } else if (level === LEVEL.TOWN) {
+    return ['village', 'county'];
+  } else if (level === LEVEL.VILLAGE) {
+    return ['county', 'town'];
+  }
+  return [];
+}
+
 export default function Map() {
-  const [level, setLevel] = useState<LEVEL>(LEVEL.TOWN);
+  const responsive = useResponsive();
   const map = useRef<SVGSVGElement>(null);
-  const zoomHandler = (event: any) => {
-    const { transform } = event;
-    d3.select(map.current)
-      .select('g')
-      .attr('transform', transform)
-      .attr('stroke-width', 1 / transform.k);
+  const [mapData, setMapData] = useState<{
+    county: Feature<Geometry, GeoJsonProperties>[];
+    town: Feature<Geometry, GeoJsonProperties>[];
+    village: Feature<Geometry, GeoJsonProperties>[];
+  }>({
+    county: [],
+    town: [],
+    village: [],
+  });
+  const [level, setLevel] = useState<LEVEL>(LEVEL.COUNTY);
+  const [scale, setScale] = useState<number>(5500);
+  const [currentSelectArea, setCurrentSelectArea] = useState<{
+    county: string;
+    town: string;
+    village: string;
+  }>({
+    county: '',
+    town: '',
+    village: '',
+  });
+
+  const setMapScaleScope = (responsive: Record<string, boolean>) => {
+    if (responsive['large']) {
+      setScale(9000);
+    } else if (responsive['middle']) {
+      setScale(7000);
+    } else if (responsive['small']) {
+      setScale(5500);
+    }
+  };
+
+  const setNextLevel = (level: LEVEL) => {
+    if (level === LEVEL.COUNTY) {
+      setLevel(LEVEL.TOWN);
+    } else if (level === LEVEL.TOWN) {
+      setLevel(LEVEL.VILLAGE);
+    }
+  };
+
+  const resetLevel = () => {
+    setLevel(LEVEL.COUNTY);
   };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const draw = async (level: LEVEL) => {
-    const zoom = d3.zoom().scaleExtent([1, 4]).on('zoom', zoomHandler);
-    const mapContainer = d3.select(map.current);
-    mapContainer.call(zoom as any);
-    const g = mapContainer.append('g');
-    const projectMethod = d3.geoMercator().center([121, 24]).scale(8400);
-    const pathGenerator = d3.geoPath().projection(projectMethod);
+  const getMapData = async (
+    level: LEVEL,
+    currentSelectArea: {
+      county: string;
+      town: string;
+      village: string;
+    },
+  ) => {
     const url = levelMap[level];
-    const mapData = (await d3.json(url)) as any;
-    const geometries = topojson.feature(
-      mapData,
-      mapData.objects['TAIWAN'],
-    ) as unknown as FeatureCollection;
-    g.append('path');
-    const paths = g.selectAll('path').data(geometries.features);
-    paths
-      .enter()
+    // 設定地圖資料 以利後續使用 不用再次取得
+    if (level === LEVEL.COUNTY) {
+      const mapData = (await d3.json(url)) as unknown as any;
+      const geometries = topojson.feature(
+        mapData,
+        mapData.objects['TAIWAN'],
+      ) as unknown as FeatureCollection;
+      setMapData((prev) => ({
+        ...prev,
+        county: geometries.features,
+        town: [],
+        village: [],
+      }));
+    }
+    if (level === LEVEL.TOWN) {
+      const mapData = (await d3.json(url)) as unknown as any;
+      const geometries = topojson.feature(
+        mapData,
+        mapData.objects['TAIWAN'],
+      ) as unknown as FeatureCollection;
+      const townMapData = geometries.features.filter((item) => {
+        return item.properties?.['COUNTYNAME'] === currentSelectArea.county;
+      });
+      setMapData((prev) => ({
+        ...prev,
+        town: townMapData,
+        village: [],
+      }));
+    }
+    if (level === LEVEL.VILLAGE) {
+      const mapData = (await d3.json(url)) as unknown as any;
+      const geometries = topojson.feature(
+        mapData,
+        mapData.objects['TAIWAN'],
+      ) as unknown as FeatureCollection;
+      const villageMapData = geometries.features.filter((item) => {
+        return (
+          item.properties?.['COUNTYNAME'] === currentSelectArea.county &&
+          item.properties?.['TOWNNAME'] === currentSelectArea.town
+        );
+      });
+      setMapData((prev) => ({
+        ...prev,
+        village: villageMapData,
+      }));
+    }
+  };
+
+  const currentMapData = useMemo(() => {
+    if (level === LEVEL.COUNTY) {
+      return mapData.county;
+    } else if (level === LEVEL.TOWN) {
+      return mapData.town;
+    } else if (level === LEVEL.VILLAGE) {
+      return mapData.village;
+    }
+    return [];
+  }, [level, mapData.county, mapData.town, mapData.village]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const draw = async (
+    mapEl: SVGSVGElement,
+    level: LEVEL,
+    scale: number,
+    features: Feature<Geometry, GeoJsonProperties>[],
+  ) => {
+    if (!features.length) return;
+    const width = mapEl.clientWidth;
+    const height = mapEl.clientHeight;
+    const zoom = d3
+      .zoom()
+      .scaleExtent([1, 8])
+      .on('zoom', function (event) {
+        const { transform } = event;
+        d3.select(map.current)
+          .selectAll('path')
+          .attr('transform', transform)
+          .attr('stroke-width', 1 / transform.k);
+      });
+    const mapContainer = d3
+      .select(map.current)
+      .attr('style', 'width: 100%; height: 100%')
+      .on('click', reset)
+      .call(zoom as any);
+    const projectMethod = d3.geoMercator().center([121, 24]).scale(scale);
+    const pathGenerator = d3.geoPath().projection(projectMethod);
+    const restClassNames = getRestClassNames(level);
+    restClassNames.forEach((className) => {
+      mapContainer.selectAll(`.${className} path`).style('opacity', '0');
+    });
+    mapContainer
+      .selectAll(`.${level}`)
+      .data(features)
+      .join('g')
       .append('path')
+      .attr('cursor', 'pointer')
       .attr('d', pathGenerator)
-      .attr('style', 'fill: #ccc; stroke: #fff; stroke-width: 1px;')
+      .attr('style', getStyles(level))
+      .on('click', function (event, d) {
+        clickTransition(event, d);
+        setNextLevel(level);
+        setCurrentSelectArea({
+          county: d.properties?.['COUNTYNAME'],
+          town: d.properties?.['TOWNNAME'] || '',
+          village: d.properties?.['VILLNAME'] || '',
+        });
+      })
       .append('title')
-      .text((d) => d.properties?.['COUNTYNAME']);
+      .text((d) => getTitle(level, d));
+    function clickTransition(event: any, d: any) {
+      event.stopPropagation();
+      const [[x0, y0], [x1, y1]] = pathGenerator.bounds(d);
+      mapContainer
+        .transition()
+        .duration(750)
+        .call(
+          zoom.transform as any,
+          d3.zoomIdentity
+            .translate(width / 2, height / 2)
+            .scale(
+              Math.min(
+                8,
+                0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height),
+              ),
+            )
+            .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+          d3.pointer(event, mapContainer.node()),
+        );
+    }
+    function reset() {
+      const zoomTransform = mapContainer.node() as SVGSVGElement;
+      resetLevel();
+      mapContainer
+        .transition()
+        .duration(750)
+        .call(
+          zoom.transform as any,
+          d3.zoomIdentity,
+          d3.zoomTransform(zoomTransform).invert([width / 2, height / 2]),
+        );
+    }
   };
 
   useEffect(() => {
-    draw(level);
-  }, [level, draw]);
-  return <svg className="w-full h-full" ref={map}></svg>;
+    getMapData(level, currentSelectArea);
+  }, [level, currentSelectArea]);
+
+  useEffect(() => {
+    if (map.current) {
+      draw(map.current, level, scale, currentMapData);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [level, scale, currentMapData]);
+
+  useEffect(() => {
+    responsive && setMapScaleScope(responsive);
+  }, [responsive]);
+
+  return (
+    <svg className="w-full h-full" ref={map}>
+      {mapData.county.length &&
+        mapData.county.map((item) => {
+          return (
+            <g
+              key={item.properties?.COUNTYCODE}
+              className="county"
+              id={item.properties?.COUNTYNAME}
+            ></g>
+          );
+        })}
+      {mapData.town.length &&
+        mapData.town.map((item) => {
+          return (
+            <g
+              key={item.properties?.TOWNCODE}
+              className="town"
+              id={item.properties?.TOWNNAME}
+            ></g>
+          );
+        })}
+
+      {mapData.village.length &&
+        mapData.village.map((item) => {
+          return (
+            <g
+              key={item.properties?.VILLCODE}
+              className="village"
+              id={item.properties?.VILLNAME}
+            ></g>
+          );
+        })}
+    </svg>
+  );
 }
